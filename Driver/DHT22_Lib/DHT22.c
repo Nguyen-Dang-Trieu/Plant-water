da8438c1c9ecf784e3d60ade41a64b2bf5bb4915
Nguyen dang trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2025-06-30 13:08:43 +0700

    Update DHT22.c

--- Driver/DHT22_Lib/DHT22.c
+++ Driver/DHT22_Lib/DHT22.c
@@ -5,17 +5,17 @@
  *  @param  usec Optionally pass pull-up time (in microseconds) before DHT reading 
  *         
  */
-void DHT22_Init(uint8_t usec, uint8_t pin, struct DHT22 sensor)
+void DHT22_Init(uint8_t usec, uint8_t pin, struct DHT22* sensor)
 {
-  pinMode(pin, INPUT_PULLUP); // Data-bus's free status is high voltage level
-
-  /* Using this value makes sure that millis() - lastreadtime will be
-   * >= MIN_INTERVAL right away. Note that this assignment wraps around, but so will the subtraction.
-   */ 
-  _lastreadtime = millis() - MIN_INTERVAL;
+  sensor->pin = pin;
+  GPIOx_ConfigMode (&DDRD, &PORTD, pin, INPUT_PULLUP);   // Data-bus's free status is high voltage level
+  
+  sensor->lastReadTime = millis() - MIN_INTERVAL;
 
   // Time (in usec) to pull up data line before reading , 
-  pullTime = usec;
+  sensor->pullTime = usec;
+
+  sensor->maxCycles = microsecondsToClockCycles(1000); // 1 millisecond timeout for reading pulses from DHT sensor.
 }
 
 /*!
@@ -27,23 +27,23 @@ void DHT22_Init(uint8_t usec, uint8_t pin, struct DHT22 sensor)
  *
  *  @return Temperature value in Celsius
  */
-float DHT22_ReadTemperature(bool select, struct DHT22 sensor)
+float DHT22_ReadTemperature(bool select, struct DHT22* sensor)
 {
   float f = NAN;
 
-  if (read()) { // Check if there is data
-    f = ((uint16_t)(data[2] & 0x7F)) << 8 | data[3]; // Bit 15 indicates sign, clear it to get the raw temperature
+  if (read(sensor)) { // Check if there is data
+    f = ((uint16_t)(sensor->data[2] & 0x7F)) << 8 | sensor->data[3]; // Bit 15 indicates sign, clear it to get the raw temperature
     
     /* Temperature resolution of 16Bit, the previous high; temperature sensor string value is 10 times
     the actual temperature value; - datasheet*/ 
     f *= 0.1; 
 
-    if (data[2] & 0x80) { 
+    if (sensor->data[2] & 0x80) { 
       //  Negative temperature
       f *= -1;
     }
     
-    if (S) {
+    if (select) {
       f = convertCtoF(f);       
     }
   }
@@ -56,18 +56,18 @@ float DHT22_ReadTemperature(bool select, struct DHT22 sensor)
  *          value in Celsius
  *  @return float value in Fahrenheit
  */  
-float DHT22_ConvertCtoF(float celsius, struct DHT22 sensor) { return celsius * 1.8 + 32; }
-float DHT22_ConvertFtoC(float fahrenheit, struct DHT22 sensor) { return (fahrenheit - 32) * 0.55555; }   
+float DHT22_ConvertCtoF(float celsius, struct DHT22* sensor) { return celsius * 1.8 + 32; }
+float DHT22_ConvertFtoC(float fahrenheit, struct DHT22* sensor) { return (fahrenheit - 32) * 0.55555; }   
 
 /*!
  *  @brief  Read Humidity
  *
  *  @return float value - humidity in percent
  */
-float DHT22_ReadHumidity(struct DHT22 sensor) {
+float DHT22_ReadHumidity(struct DHT22* sensor) {
   float f = NAN;
-  if (read()) { // Check if there is data
-    f = ((word)data[0]) << 8 | data[1];
+  if (read(sensor)) { // Check if there is data
+    f = ((word) sensor->data[0]) << 8 | sensor->data[1];
     f *= 0.1;
   }
   return f;
@@ -79,48 +79,49 @@ float DHT22_ReadHumidity(struct DHT22 sensor) {
  *
  *  @return float value
  */
-bool read(struct DHT22 sensor)
+bool read(struct DHT22* sensor)
 {
   // Check if sensor was read less than two seconds ago and return early to use last reading.
   uint32_t currenttime = millis();
-  if (!force && ((currenttime - _lastreadtime) < MIN_INTERVAL)) {
-    return _lastresult; // return last correct measurement
+  if ((currenttime - sensor->lastReadTime) < MIN_INTERVAL) {
+    return sensor->lastResult; // return last correct measurement
   }
-  _lastreadtime = currenttime;
+  sensor->lastReadTime = currenttime;
 
   // Reset 40 bits of received data to zero.
-  data[0] = data[1] = data[2] = data[3] = data[4] = 0;
+  sensor->data[0] = sensor->data[1] = sensor->data[2] = sensor->data[3] = sensor->data[4] = 0;
 
   // Go into high impedence state to let pull-up raise data line level and start the reading process.
-  pinMode(_pin, INPUT_PULLUP);
+  GPIOx_ConfigMode (&DDRD, &PORTD, sensor->pin, INPUT_PULLUP);
   delay(1);
 
   // First set data line low for a period according to DHT22 sensor 
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, LOW);
+  GPIOx_ConfigMode (&DDRD, &PORTD, sensor->pin, OUTPUT);
+  GPIOx_Write(&PORTD, sensor->pin,  LOW);
+
   delayMicroseconds(1100); // datasheet says "at least 1ms"
 
 
   uint32_t cycles[80];                                    // array: each element is used to hold the LOW or HIGH time period
   {
     // End the start signal by setting data line high for 40 microseconds.
-    pinMode(_pin, INPUT_PULLUP);
+    GPIOx_ConfigMode (&DDRD, &PORTD, sensor->pin, INPUT_PULLUP);
 
     // Delay a moment to let sensor pull data line low.
-    delayMicroseconds(pullTime);
+    delayMicroseconds(sensor->pullTime);
 
     // Now start reading the data line to get the value from the DHT22 sensor.
 
     // First expect a low signal for ~80 microseconds followed by a high signal, for ~80 microseconds again.
     if (expectPulse(LOW) == TIMEOUT) {
       Serial.println(F("DHT timeout waiting for start signal low pulse."));
-      _lastresult = false;
-      return _lastresult;
+      sensor->lastResult = false;
+      return sensor->lastResult;
     }
     if (expectPulse(HIGH) == TIMEOUT) {
       Serial.println(F("DHT timeout waiting for start signal high pulse."));
-      _lastresult = false;
-      return _lastresult;
+      sensor->lastResult = false;
+      return sensor->lastResult;
     }
 
     /*Giải thích thêm:
@@ -137,25 +138,26 @@ bool read(struct DHT22 sensor)
 
   // Inspect pulses and determine which ones are 0 (high state cycle count < low
   // state cycle count), or 1 (high state cycle count > low state cycle count).
-  for (int i = 0; i < 40; ++i) { // sau vòng lặp for này thì ta đã có dữ liệu trong các byte data
+  for (int i = 0; i < 40; ++i) { 
     uint32_t lowCycles = cycles[2 * i];
     uint32_t highCycles = cycles[2 * i + 1];
 
     
     if ((lowCycles == TIMEOUT) || (highCycles == TIMEOUT)) {
       Serial.println(F("DHT timeout waiting for pulse."));
-      _lastresult = false;
-      return _lastresult;
+      sensor->lastResult = false;
+      return sensor->lastResult;
     }
 
     /* Giải thích tại sao lại là data[i/8]
      *  Vì khi i chạy từ 0 -> 39 chia làm 5 byte (tương ứng với data[0] = 1byte -> data[4] = 1byte khi chia i/8
      */
-    data[i / 8] <<= 1;                                  // data[i/8] = data[i/8] << 1;
+    sensor->data[i / 8] <<= 1;    
+                                  
     // Now compare the low and high cycle times to see if the bit is a 0 or 1.
     if (highCycles > lowCycles) {
       // High cycles are greater than 50us low cycle count, must be a 1.
-      data[i / 8] |= 1;                                // data[i/8] = data[i/8] | 1 ;
+      sensor->data[i / 8] |= 1;                                
     }
     // Else high cycles are less than (or equal to, a weird case) the 50us low
     // cycle count so this must be a zero.  Nothing needs to be changed in the
@@ -163,23 +165,24 @@ bool read(struct DHT22 sensor)
   }
 
   /* ---- CHECK DATA ĐÃ NHẬN = BYTE PARITY BIT HAY KHÔNG ----*/ 
-  if (data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
-    _lastresult = true;
-    return _lastresult;
+  if (sensor->data[4] == ((sensor->data[0] + sensor->data[1] + sensor->data[2] + sensor->data[3]) & 0xFF)) {
+    sensor->lastResult = true;
+    return sensor->lastResult;
   } else {
-    _lastresult = false;
-    return _lastresult;
+    sensor->lastResult = false;
+    return sensor->lastResult;
   }
 }
 
-uint32_t DHT::expectPulse(bool level, struct DHT22 sensor) {
+uint32_t expectPulse(bool level, struct DHT22* sensor) {
   uint16_t count = 0; 
 
 // On AVR platforms use direct GPIO port access as it's much faster and better
 // for catching pulses that are 10's of microseconds in length:
-  uint8_t portState = level ? _bit : 0;
-  while ((*portInputRegister(_port) & _bit) == portState) {
-    if (count++ >= _maxcycles) {
+  uint8_t mask = (1 << sensor->pin);
+  uint8_t portState = level ? mask : 0;
+  while ((*portInputRegister(&PORTD) & mask) == portState) {
+    if (count++ >= sensor->maxCycles) {
       return TIMEOUT; // Exceeded timeout, fail.
     }
   }
