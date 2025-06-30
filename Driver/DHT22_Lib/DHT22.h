5953cc01ea8da418501866e09787ca653ec2aa87
Nguyen dang trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2025-06-30 13:08:17 +0700

    Update DHT22.h

--- Driver/DHT22_Lib/DHT22.h
+++ Driver/DHT22_Lib/DHT22.h
@@ -6,19 +6,19 @@
 
 struct DHT22 {
   uint8_t data[5];
-  uint8_t DHT22_pin;
+  uint8_t pin;
   uint32_t lastReadTime, maxCycles;
   bool lastResult;
   uint8_t pullTime; // Time (in usec) to pull up data line before reading 
 }
 
-void DHT22_Init(uint8_t usec, uint8_t pin, struct DHT22 sensor);
-float DHT22_ReadTemperature(bool select, struct DHT22 sensor);
-float DHT22_ConvertCtoF(float celsius, struct DHT22 sensor);
-float DHT22_ConvertFtoC(float fahrenheit, struct DHT22 sensor);
-float DHT22_ReadHumidity(struct DHT22 sensor);
-bool read(struct DHT22 sensor);
-uint32_t expectPulse(bool level, struct DHT22 sensor);
+void DHT22_Init(uint8_t usec, uint8_t pin, struct DHT22* sensor);
+float DHT22_ReadTemperature(bool select, struct DHT22* sensor);
+float DHT22_ConvertCtoF(float celsius, struct DHT22* sensor);
+float DHT22_ConvertFtoC(float fahrenheit, struct DHT22* sensor);
+float DHT22_ReadHumidity(struct DHT22* sensor);
+bool read(struct DHT22* sensor);
+uint32_t expectPulse(bool level, struct DHT22* sensor);
 
 
 
