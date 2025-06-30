a0861e1132d6c70f7ae32731da9e9260bf189b1a
Nguyen dang trieu <121329547+Nguyen-Dang-Trieu@users.noreply.github.com>
2025-06-30 13:10:06 +0700

    Update gpio.h

--- BareMetal_Peripherals/gpio.h
+++ BareMetal_Peripherals/gpio.h
@@ -5,14 +5,15 @@
 #include "avr/io.h"
 
 // Defines mode for PIN
-#define OUTPUT 0
-#define INPUT  1
+#define OUTPUT       0
+#define INPUT        1
+#define INPUT_PULLUP 2
 
 // Defines the logic output to pin
 #define HIGH   1
 #define LOW    0
 
-void GPIOx_ConfigMode(volatile uint8_t* DDRx, uint8_t PIN, bool MODE);
+void GPIOx_ConfigMode(volatile uint8_t* DDRx, volatile uint8_t* PORTx, uint8_t PIN, bool MODE);
 void GPIOx_Write(volatile uint8_t* PORTx, uint8_t PIN,  bool LOGIC);
 void GPIOx_WriteToggle(volatile uint8_t* PORTx, uint8_t PIN);
 bool GPIOx_Read(volatile uint8_t* PIN_REG, uint8_t PIN);
